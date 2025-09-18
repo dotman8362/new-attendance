69d300ce3f3d492a6843ea28b1cbc578802e8adb
dotman8362 <118911062+dotman8362@users.noreply.github.com>
2025-09-18 14:17:48 +0100

    Update register.js

--- Frontend/js/register.js
+++ Frontend/js/register.js
@@ -58,7 +58,7 @@ document.getElementById('registerForm').addEventListener('submit', async functio
     };
 
     try {
-        const response = await fetch('http://localhost:5000/api/auth/register', {
+        const response = await fetch('/api/auth/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
